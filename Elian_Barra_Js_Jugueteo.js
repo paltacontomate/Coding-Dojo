fba92b0efa497265a43106e9b7ed84a68423c78a
paltacontomate <elian.barra.millaqueo@alumnos.sip.cl>
2022-04-21 09:23:31 -0400

    DjavaScript
    Signed-off-by: paltacontomate <elian.barra.millaqueo@alumnos.sip.cl>

---
+++ Elian_Barra_Js_Jugueteo.js
@@ -0,0 +1,9 @@
+function muestraSIELNiñoPuedeSubirALaMontañaRusa(alturaNiño) {
+    if (alturaniño >52)
+    {         
+    console.log("¡Subete, chico!")
+    }
+    else{
+ console.log("Lo siento,chico. Tal vez el proximo año") 
+        }
+    }
